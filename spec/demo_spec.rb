c6635345f498eb2856373df333ecfb7a16eed086
sawada <sawada@csplan.jp>
2023-01-02 03:59:50 +0900

    fix

--- spec/demo_spec.rb
+++ spec/demo_spec.rb
@@ -1,7 +1,12 @@
+require 'rails_helper'
+
 describe 'demo' do
     it 'hello' do
         a = true
         expect(a).to eq true
     end
 
+    it 'isTest' do
+        expect(Rails.env).to eq 'test'
+    end
 end
