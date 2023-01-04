f300d03b4ce599cfe11553269e14b5cfb4d2d616
sawada <sawada@csplan.jp>
2023-01-05 03:56:37 +0900

    add system spec

---
+++ spec/system/tasks_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe 'demo system spec', type: :system do
+    before do
+        driven_by(:rack_test)
+    end
+
+    scenario 'demo' do
+        # 指定のコントローラーのアクションにアクセス
+        visit 'tasks#index'
+        # 指定の文字列のリンクをクリック
+        click_link 'New task'
+        # form に記載する
+        fill_in 'Title', with: 'test task'
+        fill_in 'Description', with: 'test task desc'
+        fill_in 'Due date', with: '2022-11-11'
+        # submit
+        click_button 'Create Task'
+
+        # task が追加されたことを確認する
+        tasks = Task::all
+        expect(tasks.count).to eq 1
+    end
+end
